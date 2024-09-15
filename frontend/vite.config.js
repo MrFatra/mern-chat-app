e5381689080cfc404c1e5c0e998bf07cfb75f3d4
MrFatra <ramadhanfatra2004@gmail.com>
2024-09-15 10:10:51 +0700

    config init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
   server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
     proxy: {
-      '/api': { target: 'http://localhost:3000' }
+      // '/api': { target: 'http://127.0.0.1:3000'}
+      '/api': { target: 'http://backend:3000'}
     }
   }
 })
