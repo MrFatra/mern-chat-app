e2b7ba026d10e0df9b1d399efc88f3d55e7ae568
MrFatra <ramadhanfatra2004@gmail.com>
2024-09-15 11:12:50 +0700

    fix: messages reset after logout

--- frontend/src/hooks/logout.js
+++ frontend/src/hooks/logout.js
@@ -2,10 +2,12 @@ import { useState } from "react"
 import { USER_AUTH_KEY } from "../config"
 import { useAuthContext } from "../context/Auth"
 import toast from "react-hot-toast"
+import useConversation from "../store/useConversation"
 
 export const useLogout = () => {
     const [loading, setLoading] = useState(false)
     const { setAuthUser } = useAuthContext()
+    const { setSelectedContact, setMessages } = useConversation()
 
     const logout = async () => {
         setLoading(true)
@@ -14,6 +16,8 @@ export const useLogout = () => {
             const result = await request.json()
             if (!request.ok) throw new Error(result.message)
             localStorage.removeItem(USER_AUTH_KEY)
+            setSelectedContact(null)
+            setMessages([])
             setAuthUser(null)
             toast.success(result.message)
         } catch (err) {
